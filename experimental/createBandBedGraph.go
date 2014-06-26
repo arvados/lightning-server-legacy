9264a53da6b33d81cf74573250362209f67fa9ee
abram <abram@curoverse.com>
2014-06-26 14:57:57 -0400

    updating to use bioenv

--- experimental/createBandBedGraph.go
+++ experimental/createBandBedGraph.go
@@ -3,44 +3,52 @@ package main
 import "os"
 import "fmt"
 import "os/exec"
-import _ "log"
+//import "log"
+
+import "flag"
 
 import "./aux"
 import "./bioenv"
 
-var chr =
-  []string{ "chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10",
-            "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20",
-            "chr21", "chr22", "chrX", "chrY", "chrM" }
+var chr = []string{ "chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10",
+                    "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20",
+                    "chr21", "chr22", "chrX", "chrY", "chrM" }
 
-func usage() {
-  fmt.Println("usage:")
-  fmt.Println("  createBandBedGraph <destDir>")
+
+var benv bioenv.BioEnvContext
+
+var g_bw2bedGraph       *string
+var g_cytomapFilename   *string
+var g_bigWigFilename    *string
+var g_bedGraphDestDir   *string
+
+func init() {
+  var err error
+  benv, err = bioenv.BioEnv()
+  if err != nil { panic(err) }
+
+  g_bw2bedGraph     = flag.String( "bigWigToBedGraph", benv.Env["bigWigToBedGraph"], "Location of the bigWigToBedGraph executable")
+  g_cytomapFilename = flag.String( "cytoBand", benv.Env["cytoBand"], "Location of the cytoBand file")
+  g_bigWigFilename  = flag.String( "bigWig", benv.Env["wgEncodeCrgMapabilityAlign24mer.bw"], "Location of the bigWig mapability file")
+  g_bedGraphDestDir = flag.String( "destination-dir", "", "Destination Directory" )
+
+  flag.Parse()
+  benv.ProcessFlag()
+
+  if len(*g_bedGraphDestDir)==0 {
+    fmt.Fprintf( os.Stderr, "Provide destination directory\n" )
+    flag.PrintDefaults()
+    os.Exit(2)
+  }
+
 }
 
 func main() {
 
   bufferWindow := 0
-
-  benv,err := bioenv.BioEnv()
-  if err != nil { panic(err) }
-
-  if len(os.Args) != 2 {
-    fmt.Println("Provide destination output directory")
-    usage()
-    os.Exit(0)
-  }
-
-
-  bigWigToBedGraph := benv["bigWigToBedGraph"]
-  wgEncode24merFn  := benv["wgEncodeCrgMapabilityAlign24mer.bw"]
-  cytomapFilename  := benv["cytoBand"]
-  bedGraphDestDir  := os.Args[1]
-
-
   bandBounds := make( map[string]map[int][2]int )
 
-  aux.BuildBandBounds( bandBounds, cytomapFilename )
+  aux.BuildBandBounds( bandBounds, *g_cytomapFilename )
 
   // Create bedGraph files, one per band
   //
@@ -84,9 +92,11 @@ func main() {
       opt1 := fmt.Sprintf("-chrom=%s", string(chrom) )
       opt2 := fmt.Sprintf("-start=%d", s)
       opt3 := fmt.Sprintf("-end=%d", e)
-      outfile := fmt.Sprintf("%s/%s_band%d_s%d_e%d.bedGraph", bedGraphDestDir, chrom, j, s, e )
+      //infile := fmt.Sprintf("/scratch/%s/wgEncodeCrgMapabilityAlign24mer.bw", user )
+      infile := *g_bigWigFilename
+      outfile := fmt.Sprintf("%s/%s_band%d_s%d_e%d.bedGraph", *g_bedGraphDestDir, chrom, j, s, e )
 
-      oput, err := exec.Command( bigWigToBedGraph, opt1, opt2, opt3, wgEncode24merFn, outfile ).CombinedOutput()
+      oput, err := exec.Command( *g_bw2bedGraph , opt1, opt2, opt3, infile, outfile ).CombinedOutput()
       _ = oput
       if err != nil { panic(err) }
 
