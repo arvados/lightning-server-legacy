4dcc462a713d1350c5839dddec134f7176f8f7f8
sguthrie <sallyeguthrie@gmail.com>
2014-09-03 10:07:10 -0400

    Alter STATIC_URL to point to /var/www

--- experimental/pylightweb/lightning/lightning/settings.py
+++ experimental/pylightweb/lightning/lightning/settings.py
@@ -96,6 +96,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/var/www/pylightweb/static/'
 
 STATIC_ROOT = "/var/www/pylightweb/static"
