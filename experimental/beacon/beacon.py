3623375e695fb969fac98637646a8f468c1bebb4
Nancy Ouyang <nancy.ouyang@gmail.com>
2014-09-22 15:57:48 -0400

    added support for searching across 2 tiles for multiple alleles, no indels

--- experimental/beacon/beacon.py
+++ experimental/beacon/beacon.py
@@ -1,5 +1,5 @@
 # all the imports
-import os, sys
+import os, sys, string
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from contextlib import closing
@@ -62,14 +62,14 @@ def findtilevar(genome, path, step):
         tiles = f.read().split(' ')[1:] #[1: get rid of genome name at beginning of ABV
         tiles = dict(zip(tiles[::2], tiles[1::2])) #turn it from '0', 'seq0', '1', 'seq1' to a dictionary with keys 0, 1, ...
         tile = tiles[path]
-        var = tile[int(step)]
+        var = tile[int(step):int(step)+2] #retrieve exactly two tiles
         return var
 
-def findallele(tile_id, tilevar, coordinate, begin_int, lenalleles):
+def findseq(tilevar, tile_id):
     if tilevar == '-':
-        return 'No call'
+        return False, 'No call'
     elif tilevar == '#':
-        return 'No data available (lost in encoding as ABV file)' 
+        return False, 'No data available (lost in encoding as ABV file)' 
     elif tilevar == '.':
         tilevarname = int(tile_id + '000', 16)
     #at some point, need to deal with "complex" cases
@@ -77,12 +77,38 @@ def findallele(tile_id, tilevar, coordinate, begin_int, lenalleles):
         foo =  a2base64index(tilevar)-2
         tilevarname = tile_id + foo.zfill(3)
         tilevarname = int(tilevarname, 16)
-    #cursor = g.db.execute('SELECT * FROM loadgenomes_tilevariant WHERE %s = tile_variant_name', [tilevarname])
     cursor = g.db.execute('SELECT * FROM loadgenomes_tilevariant WHERE %s = tile_variant_name', tilevarname)
     row = cursor.fetchone()
     seq = row['sequence']
-    index = coordinate - begin_int
-    return seq[index:index+lenalleles]
+    return True, seq
+
+def findallele(tile_id, tilevars, coordinate, begin_ints, lenalleles):
+    foundseq, msg = findseq(tilevars[0], tile_id)
+    if DEBUG: print 'foundseq, msg', foundseq, msg
+    if not foundseq:
+        return False, msg
+    else:
+        seq = msg
+        index = coordinate - begin_ints[0]
+        if DEBUG: print 'index, coordinate, begin_ints', index, coordinate, begin_ints
+        if lenalleles <= len(seq[index:]): #if search seq fits in one tile 
+            alleles = seq[index:index+lenalleles]
+            if DEBUG: print 'searchseq fit on one tile', lenalleles, len(seq[index:])
+        else: #otherwise, we read the next tilevar and append to our current list of alleles
+            alleles = seq[index:begin_ints[1]]
+            tile_id = format(int(tile_id, 16)+1, 'x')
+            if DEBUG: print 'tile_id +1', tile_id
+            foundseq, msg = findseq(tilevars[1], tile_id) #the next tile_id, one step up
+            if not foundseq:
+                return False, msg
+            else:
+                #remove the parts that are overlapping, aka uppercase
+                seq = msg.lstrip(string.uppercase)
+                num_addtl = lenalleles - len(alleles)
+                seq = seq[:num_addtl]
+                if DEBUG: print 'two tiles', lenalleles, coordinate, num_addtl, alleles, seq, msg
+                alleles += seq
+    return True, alleles
 
 def findtileid(search_coord, search_chrom):
     try:
@@ -91,13 +117,22 @@ def findtileid(search_coord, search_chrom):
     except ValueError:
         iswonky = True
     if iswonky:
-        cursor = g.db.execute('SELECT * FROM loadgenomes_tilelocusannotation WHERE %s >= begin_int AND %s <= end_int AND chromosome = %s AND chromosome_name = %s LIMIT 1', [search_coord, search_coord, wonkychrom, search_chrom])
+        cursor = g.db.execute('SELECT * FROM loadgenomes_tilelocusannotation WHERE %s >= begin_int AND %s < end_int AND chromosome = %s AND chromosome_name = %s LIMIT 1', [search_coord, search_coord, wonkychrom, search_chrom])
         row = cursor.fetchone()
     else:
         search_chrom_name = ''
-        cursor = g.db.execute('SELECT * FROM loadgenomes_tilelocusannotation WHERE %s >= begin_int AND %s <= end_int AND chromosome = %s LIMIT 1', [search_coord, search_coord, search_chrom])
+        cursor = g.db.execute('SELECT * FROM loadgenomes_tilelocusannotation WHERE %s >= begin_int AND %s < end_int AND chromosome = %s LIMIT 1', [search_coord, search_coord, search_chrom])
         row = cursor.fetchone()
-    return row
+    if row == None:
+        return None, None
+    tile_id = format(row['tile_id'], 'x') #1c4000002
+    begin_ints = []
+    begin_ints.append(row['begin_int'])
+    secondid = int(row['id'])+1
+    cursor = g.db.execute('SELECT * FROM loadgenomes_tilelocusannotation WHERE %s = id LIMIT 1', [secondid])
+    row = cursor.fetchone()
+    begin_ints.append(row['begin_int'])
+    return tile_id, begin_ints
 
 def search(search_pop, search_gen, search_chrom, search_coord, search_allele):
     flashmsg = None
@@ -119,13 +154,11 @@ def search(search_pop, search_gen, search_chrom, search_coord, search_allele):
         return flashmsg, msg 
 
     #Begin Search
-    row = findtileid(search_coord, search_chrom)
-    if row == None:
+    tile_id, begin_ints = findtileid(search_coord, search_chrom)
+    if tile_id == None:
         msg['msg'] = 'Error: Is your coordinate valid? No allele(s) found at the coordinate ' + str(search_coord) + ' on chromosome ' + \
             str(search_chrom) + ' with reference genome ' + search_gen + ' for at least one of the genomes in this population.'
         return flashmsg, msg 
-    tile_id = format(row['tile_id'], 'x') #1c4000002
-    begin_int = row['begin_int']
 
     path = tile_id[:3] #first three digits
     step = tile_id[-2:] #last two digits
@@ -134,13 +167,18 @@ def search(search_pop, search_gen, search_chrom, search_coord, search_allele):
     debugmsg = []
     lenalleles = len(search_allele)
     for abv in abvfnames:
-        tilevar = findtilevar(abv, path, step)
-        alleles = findallele(tile_id, tilevar, search_coord, begin_int, lenalleles)
-        debuginfo = [abv, path, step, tilevar, alleles, tile_id, begin_int]
+        #hardcoded, we search across at most 2 tiles right now
+        tilevars = findtilevar(abv, path, step)
+        foundallele, alleles = findallele(tile_id, tilevars, search_coord, begin_ints, lenalleles)
+        listvars = 'abv, path, step, tilevars, alleles, tile_id, begin_ints, lenalleles\n'
+        debuginfo = [abv, path, step, tilevars, alleles, tile_id, begin_ints, lenalleles]
         debugmsg.append(debuginfo)
-        if (alleles.lower() == search_allele.lower()):
-            count += 1
-
+        if not foundallele:
+            msg['msg'] = msg
+            break
+        else:
+            if (alleles.lower() == search_allele.lower()):
+                count += 1
     if count != 0:
         flashmsg = True 
     else:
@@ -149,11 +187,9 @@ def search(search_pop, search_gen, search_chrom, search_coord, search_allele):
     msg['msg'] = 'You searched for: allele "'+ search_allele + '" at coordinate "' + str(search_coord) + \
             '" (in ' + str(search_chrom) + ', ' + search_gen + ', ' + search_pop + '). \n\n' 
     if DEBUG:
-        debugmsg = "\n".join(map(lambda x: str(x), debugmsg))
+        debugmsg = listvars + "\n".join(map(lambda x: str(x), debugmsg))
         msg['debug'] = debugmsg
     return flashmsg, msg
-
-
 
 def connect_db():
     return db.engine.connect()
@@ -185,7 +221,6 @@ def search_entries():
     search_coord = request.form['search_coord']
     search_allele = request.form['search_allele']
     flashmsg, msg = search(search_pop, search_gen, search_chrom, search_coord, search_allele)
-
     if flashmsg == None:
         flash(msg['msg'])
     else:
