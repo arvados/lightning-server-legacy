d3e8e95845f701e1a2a9c22c0cc55f819c1affa6
sguthrie <sallyeguthrie@gmail.com>
2014-09-29 16:44:29 -0400

    Changed the statistics page to load via an ajax query to avoid a 504 timeout error

--- experimental/pylightweb/lightning/loadgenomes/urls.py
+++ experimental/pylightweb/lightning/loadgenomes/urls.py
@@ -4,6 +4,7 @@ from loadgenomes import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.statistics, name='statistics'),
+                       url(r'^getStats/$', views.loadstatistics, name='loadstatistics'),
                        url(r'^(?P<annotation_id>\d+)/$', views.detail, name='detail'),
                        url(r'^(?P<annotation_id>\d+)/review/$', views.review, name='review'),
                        url(r'^(?P<tile_id>\d+)/annotate/$', views.annotate, name='annotate'),
