eaa5c3332a76b6dc6494e54f692c06ceb4798651
sguthrie <sallyeguthrie@gmail.com>
2014-10-03 19:42:33 -0400

    Added application home

--- experimental/pylightweb/lightning/lightning/settings.py
+++ experimental/pylightweb/lightning/lightning/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tile_library',
-    #'home',
+    'home',
     #'loadgenomes',
     #'slippy',
     #'loadgenes',
--- experimental/pylightweb/lightning/lightning/urls.py
+++ experimental/pylightweb/lightning/lightning/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       #url(r'^', include('home.urls', namespace="home")),
+                       url(r'^', include('home.urls', namespace="home")),
                        #url(r'^slippy/', include('slippy.urls', namespace="slippy")),
                        #url(r'^loadgenes/', include('loadgenes.urls', namespace="loadgenes")),
                        url(r'^library/', include('tile_library.urls', namespace="tile_library")),
