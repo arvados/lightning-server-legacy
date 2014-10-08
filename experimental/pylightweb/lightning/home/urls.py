cdf3ac4f05c4d634c5e7ce72c1880c8a83545345
sguthrie <sallyeguthrie@gmail.com>
2014-10-08 11:45:46 -0400

    Added a help page

--- experimental/pylightweb/lightning/home/urls.py
+++ experimental/pylightweb/lightning/home/urls.py
@@ -4,4 +4,5 @@ from home import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
+                       url(r'^help/$', views.help_me, name='help'),
                        )
--- experimental/pylightweb/lightning/home/views.py
+++ experimental/pylightweb/lightning/home/views.py
@@ -5,3 +5,6 @@ from django.template import RequestContext
 
 def index(request):
     return render_to_response('home/index.html', context_instance=RequestContext(request))
+
+def help_me(request):
+    return render_to_response('home/help.html', context_instance=RequestContext(request))
