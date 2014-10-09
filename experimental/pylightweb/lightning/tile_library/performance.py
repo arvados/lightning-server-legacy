79e7327bf2bf81331c87d7acfae8e956d811cf6b
sguthrie <sallyeguthrie@gmail.com>
2014-10-09 16:19:58 -0400

    timeit problems

--- experimental/pylightweb/lightning/tile_library/performance.py
+++ experimental/pylightweb/lightning/tile_library/performance.py
@@ -1,35 +1,37 @@
 #Testing performance
 
 import timeit
-from tile_library.models import Tile, TileVariant, TileLocusAnnotation, VarAnnotation
-from tile_library import views
 
-#largest chromosome
-chrom = 2
+def setup():
+    from tile_library.models import Tile, TileVariant, TileLocusAnnotation, VarAnnotation
+    from tile_library import views
 
-#biggest path
-path = 21
+    #largest chromosome
+    chrom = 2
 
-min_pos, min_tile = views.convert_chromosome_to_tilename(chrom)
-max_pos, max_tile = views.convert_chromosome_to_tilename(chrom+1)
+    #biggest path
+    path = 21
 
-max_pos -= 1
-max_tile -= 1
+    min_pos, min_tile = views.convert_chromosome_to_tilename(chrom)
+    max_pos, max_tile = views.convert_chromosome_to_tilename(chrom+1)
 
-min_path_pos, min_path_tile = views.convert_path_to_tilename(path)
-max_path_pos, max_path_tile = views.convert_path_to_tilename(path+1)
+    max_pos -= 1
+    max_tile -= 1
 
-max_path_pos -= 1
-max_path_tile -= 1
+    min_path_pos, min_path_tile = views.convert_path_to_tilename(path)
+    max_path_pos, max_path_tile = views.convert_path_to_tilename(path+1)
+
+    max_path_pos -= 1
+    max_path_tile -= 1
 
 print "Entire Genome:"
-print "\tTile count()", timeit.timeit("Tile.objects.all().count()")
-print "\tTileVar count()", timeit.timeit("TileVariant.objects.all().count()")
+print "\tTile count()", timeit.timeit("Tile.objects.all().count()", setup="setup()")
+print "\tTileVar count()", timeit.timeit("TileVariant.objects.all().count()", setup="setup()")
 print ""
 print "Biggest Chromosome:"
-print "\tTile count()", timeit.timeit("Tile.objects.filter(tilename__range=(min_pos, max_pos)).count()")
-print "\tTileVar count()", timeit.timeit("TileVariant.objects.gilter(tile_variant_name__range=(min_tile, max_tile)).count()")
+print "\tTile count()", timeit.timeit("Tile.objects.filter(tilename__range=(min_pos, max_pos)).count()", setup="setup()")
+print "\tTileVar count()", timeit.timeit("TileVariant.objects.gilter(tile_variant_name__range=(min_tile, max_tile)).count()", setup="setup()")
 print ""
 print "Biggest Path:"
-print "\tTile count()", timeit.timeit("Tile.objects.filter(tilename__range=(min_path_pos, max_path_pos)).count()")
-print "\tTileVar count()", timeit.timeit("TileVariant.objects.filter(tile_variant_name__range(min_path_tile, max_path_tile).count()")
+print "\tTile count()", timeit.timeit("Tile.objects.filter(tilename__range=(min_path_pos, max_path_pos)).count()", setup="setup()")
+print "\tTileVar count()", timeit.timeit("TileVariant.objects.filter(tile_variant_name__range(min_path_tile, max_path_tile).count()", setup="setup()")
