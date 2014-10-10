98fe17e903baeef5a548b3db3a469db6a010b326
sguthrie <sallyeguthrie@gmail.com>
2014-10-10 10:44:36 -0400

    Added statistics table and the function to populate it/update it

---
+++ experimental/pylightweb/lightning/tile_library/generate_stats.py
@@ -0,0 +1,66 @@
+from tile_library.models import Tile, TileVariant, TileLocusAnnotation, VarAnnotation, GenomeStatistic
+from tile_library import views
+from django.db.models import Avg, Count, Max, Min
+import time
+
+def get_info(tiles, tilevars):
+    n = time.time()
+    pos_num = tiles.count()
+    x = time.time()
+    print "\tCount:", x-n
+    tile_var_info = tilevars.aggregate(tile_num=Count('tile_variant_name'),
+                                       avg_var_val=Avg('variant_value'),
+                                       max_var_val=Max('variant_value'),
+                                       min_len=Min('length'),
+                                       avg_len=Avg('length'),
+                                       max_len=Max('length')
+                                       )
+    y = time.time()
+    print "\tAggregation:", y-x
+    #annotate_info = annotations.aggregate(max_tile_ann=Max(
+    return pos_num, tile_var_info
+
+def initialize():
+    assert GenomeStatistic.objects.count() == 0
+    for i in range(27):
+        if i == 0:
+            print "Entire Genome:"
+            pos_num, tile_var_info = get_info(Tile.objects, TileVariant.objects)
+        else:
+            print "Chromosome", i, ":"
+            min_pos, min_tile = views.convert_chromosome_to_tilename(i)
+            max_pos, max_tile = views.convert_chromosome_to_tilename(i+1)
+            max_pos -= 1
+            max_tile -= 1
+            pos_num, tile_var_info = get_info(Tile.objects.filter(tilename__range=(min_pos,max_pos)),
+                                              TileVariant.objects.filter(tile_variant_name__range=(min_tile, max_tile)))
+        print tile_var_info
+        s = GenomeStatistic(statistics_type=i, position_num=pos_num, tile_num=tile_var_info['tile_num'],
+                            avg_variant_val=tile_var_info['avg_var_val'], max_variant_val=tile_var_info['max_var_val'],
+                            min_length=tile_var_info['min_len'], avg_length=tile_var_info['avg_len'],
+                            max_length=tile_var_info['max_len'])
+        s.save()
+
+def update():
+    for i in range(27):
+        if i == 0:
+            print "Entire Genome:"
+            pos_num, tile_var_info = get_info(Tile.objects, TileVariant.objects)
+        else:
+            print "Chromosome", i, ":"
+            min_pos, min_tile = views.convert_chromosome_to_tilename(i)
+            max_pos, max_tile = views.convert_chromosome_to_tilename(i+1)
+            max_pos -= 1
+            max_tile -= 1
+            pos_num, tile_var_info = get_info(Tile.objects.filter(tilename__range=(min_pos,max_pos)),
+                                              TileVariant.objects.filter(tile_variant_name__range=(min_tile, max_tile)))
+        s = GenomeStatistic.objects.get(pk=i+1)
+        assert s.statistics_type == i
+        s.position_num = pos_num
+        s.tile_num = tile_var_info['tile_num']
+        s.avg_variant_val = tile_var_info['avg_var_val']
+        s.max_variant_val = tile_var_info['max_var_val']
+        s.min_length = tile_var_info['min_len']
+        s.avg_length = tile_var_info['avg_len']
+        s.max_length = tile_var_info['max_len']
+        s.save()
