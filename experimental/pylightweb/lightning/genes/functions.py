7b481888ec7a993ff382fe656253863fa62de7f1
sguthrie <sallyeguthrie@gmail.com>
2014-10-31 14:14:07 -0400

    Added testing for genes.functions

--- experimental/pylightweb/lightning/genes/functions.py
+++ experimental/pylightweb/lightning/genes/functions.py
@@ -1,14 +1,25 @@
 #Functions for genes!
-#
-#Gene aliases with odd cases:
-#   abParts
-#   5S_rRNA (138 GeneXRef matches, none overlap, multiple chromosomes)
-#   CP
+
 from genes.models import GeneXRef, UCSC_Gene
+from tile_library.models import Tile, TileLocusAnnotation
+from django.db.models.query import QuerySet
+from django.core.exceptions import ObjectDoesNotExist
 
 def color_exons(exons, zero, total):
+    """
+        exons is list of tuples, the first integer is the start of the exon, second, the end
+        zero is int
+        total is int
+    """
     def to_percent(l):
         return (int(l)-zero)/float(total-zero)*100
+    
+    assert type(zero)==int, "Expects zero to be an integer"
+    assert type(total)==int, "Expects total to be an integer"
+    assert zero < total, "zero must be smaller than total"
+    assert zero >= 0, "zero must be greater than 0"
+    assert total > 0, "total must be greater than 0"
+
     colors = []
     covers = 0
     for i, (begin, end) in enumerate(exons):
@@ -23,6 +34,7 @@ def color_exons(exons, zero, total):
         if i == 0:
             to_append = (to_percent(begin), exon)
         else:
+            assert begin >= int(exons[i-1][1]), "Expects exons to end before a new one begins"
             to_append = (to_percent(begin)-to_percent(exons[i-1][1]), exon)
         colors.append(to_append)
         covers += sum(to_append)
@@ -37,6 +49,12 @@ def color_exons(exons, zero, total):
 
 
 def overlap(s1, e1, s2, e2):
+    assert type(s1)==int, "Expects s1 to be an integer"
+    assert type(s2)==int, "Expects s2 to be an integer"
+    assert type(e1)==int, "Expects e1 to be an integer"
+    assert type(e2)==int, "Expects e2 to be an integer"
+    assert s1 <= e1, "Expects s1 to be smaller than e1"
+    assert s2 <= e2, "Expects s2 to be smaller than e2"
     smaller = min((s1, e1), (s2, e2), key=lambda x:x[0])
     larger = max((s1, e1), (s2, e2), key=lambda x:x[0])
     if smaller[1] < larger[0]:
@@ -45,7 +63,13 @@ def overlap(s1, e1, s2, e2):
         return True
 
 def split_genes_into_groups(genes, by_tile=False):
+    assert type(genes)==QuerySet, "Expects type QuerySet for genes"
+    assert type(by_tile)==bool, "Expects type boolean for by_tile"
     overlapping_genes = []
+    try:
+        genes = genes.order_by('gene__start_tx')
+    except FieldError:
+        raise BaseException("Expects GeneXRef type of QuerySet")
     for gene in genes:
         assembly = int(gene.gene.assembly)
         chrom = int(gene.gene.chrom)
@@ -81,19 +105,106 @@ def split_genes_into_groups(genes, by_tile=False):
 def split_exons_and_get_length(genes):
     all_exons = []
     overlapping_genes = split_genes_into_groups(genes)
+    ret_genes = []
     for gene_group in overlapping_genes:
         for gene in gene_group[4:]:
+            ret_genes.append(gene)
             begins = gene.gene.exon_starts.strip(',').split(',')
             ends = gene.gene.exon_ends.strip(',').split(',')
             all_exons.append(color_exons(zip(begins, ends), gene_group[2], gene_group[3]))
-    return genes, all_exons, len(genes), len(overlapping_genes)
+    return ret_genes, all_exons, len(genes), len(overlapping_genes)
 
-def color_exon_parts(genes, tile_start, tile_end):
+def annotate_positions_with_exons_overlapping_genes(overlapping_genes, positions):
+    assert type(overlapping_genes) == list, "Expects overlapping_genes format"
+    assert type(positions) == QuerySet, "Expects QuerySet for positions"
+    assert len(overlapping_genes) <= 1, "Requires all genes to overlap"
+    if overlapping_genes != []:
+        gene_group = overlapping_genes[0]
+        assemblies = [i for i, name in TileLocusAnnotation.SUPPORTED_ASSEMBLY_CHOICES]
+        assert gene_group[0] in assemblies, "Expects overlapping_genes format. Assembly not recognized"
+        chromosomes = [i for i, name in TileLocusAnnotation.CHR_CHOICES]
+        assert gene_group[1] in chromosomes, "Expects overlapping_genes format. Chromosome not recognized"
+        assert type(gene_group[2])==int, "Expects overlapping_genes format. Start int not an integer"
+        assert type(gene_group[3])==int, "Expects overlapping_genes format. End int not an integer"
+        assert gene_group[2] <= gene_group[3], "Expects overlapping_genes format. Start int larger than end int"
+
+        all_exons = []
+        for gene_group in overlapping_genes:
+            for gene in gene_group[4:]:
+                assert type(gene)==GeneXRef, "Expects overlapping_genes format. One of the genes not GeneXRef type"
+                begins = gene.gene.exon_starts.strip(',').split(',')
+                ends = gene.gene.exon_ends.strip(',').split(',')
+                exons = [(int(begin), int(end)) for begin, end in zip(begins, ends)]
+                all_exons.extend(exons)
+        
+        all_exons = sorted(list(set(all_exons)), key=lambda x:x[0])
+        in_exon=False
+        curr_exon = 0
+        exon_dict = {}
+        try:
+            positions.order_by('tilename')
+            last_position = positions.last()
+            assert type(last_position)==Tile, "Expects positions to be of type Tile"
+            last_position_end_int = last_position.tile_locus_annotations.get(assembly=gene_group[0]).end_int
+        except ObjectDoesNotExist:
+            raise BaseException("Requires positions to have Tile Locus Annotations with the same assembly as the genes")
+        all_exons.append((last_position_end_int+10, last_position_end_int+20))
+
+        for position in positions:
+            assert type(position)==Tile, "Expects positions to be of type Tile"
+            try:
+                begin_int = position.tile_locus_annotations.get(assembly=gene_group[0]).begin_int
+                end_int = position.tile_locus_annotations.get(assembly=gene_group[0]).end_int
+            except ObjectDoesNotExist:
+                raise BaseException("Requires positions to have Tile Locus Annotations with the same assembly as the genes")
+            name = int(position.tilename)
+            while all_exons[curr_exon][1] < begin_int:
+                curr_exon += 1
+            if not in_exon:
+                if end_int < all_exons[curr_exon][0]:
+                    exon_dict[name] = in_exon #False
+                else:
+                    in_exon = True
+                    exon_dict[name] = in_exon #True
+                    if end_int - 24 > all_exons[curr_exon][1]:
+                        in_exon = False
+                        curr_exon += 1
+            else:
+                if end_int - 24 < all_exons[curr_exon][1]:
+                    exon_dict[name] = in_exon #True
+                else:
+                    exon_dict[name] = in_exon #True
+                    in_exon = False
+                    curr_exon += 1
+    else:
+        exon_dict = {}
+        for position in positions:
+            assert type(position) == Tile
+            exon_dict[int(position.tilename)] = False
+    return exon_dict
+
+def annotate_positions_with_exons(genes, positions):
     overlapping_genes = split_genes_into_groups(genes)
-    assert len(overlapping_genes) == 1, "Requires all genes to overlap"
+    return annotate_positions_with_exons_overlapping_genes(overlapping_genes, positions)
+
+def color_exon_parts(genes, tile):
+    assert type(genes) == QuerySet, "Expects genes to be of type QuerySet"
+    if type(tile) == QuerySet:
+        assert tile.count() == 1, "Expects tile to have exactly one Tile in it"
+        tile = tile.get()    
+    assert type(tile) == Tile, "Expects tile to be of type Tile (or a QuerySet of length 1 with type Tile)"
+            
     has_exons = False
     all_exons = []
     for gene in genes:
+        assert type(gene)==GeneXRef, "Expects genes to be of the type GeneXRef"
+        gene_assembly = gene.gene.assembly
+        try:
+            tile_start = tile.tile_locus_annotations.get(assembly=gene_assembly).begin_int
+            tile_end = tile.tile_locus_annotations.get(assembly=gene_assembly).end_int
+            assert gene.gene.chrom == tile.tile_locus_annotations.get(assembly=gene_assembly).chromosome, "Gene in wrong chromosome"
+        except ObjectDoesNotExist:
+            raise BaseException("Requires tile to have Tile Locus Annotations with the same assembly as the genes")
         begins = gene.gene.exon_starts.strip(',').split(',')
         ends = gene.gene.exon_ends.strip(',').split(',')
         colors = []
@@ -123,38 +234,3 @@ def color_exon_parts(genes, tile_start, tile_end):
         all_exons.append(colors)
     return has_exons, all_exons
 
-def annotate_positions_with_exons(overlapping_genes, positions):
-    assert len(overlapping_genes) == 1, "Requires all genes to overlap"
-    all_exons = []
-    for gene_group in overlapping_genes:
-        for gene in gene_group[4:]:
-            begins = gene.gene.exon_starts.strip(',').split(',')
-            ends = gene.gene.exon_ends.strip(',').split(',')
-            exons = [(int(begin), int(end)) for begin, end in zip(begins, ends)]
-            all_exons.extend(exons)
-    
-    all_exons = sorted(list(set(all_exons)), key=lambda x:x[0])
-    in_exon=False
-    curr_exon = 0
-    exon_dict = {}
-    for position in positions:
-        name = int(position.tilename)
-        while all_exons[curr_exon][1] < position.min_base:
-            curr_exon += 1
-        if not in_exon:
-            if position.max_base < all_exons[curr_exon][0]:
-                exon_dict[name] = in_exon #False
-            else:
-                in_exon = True
-                exon_dict[name] = in_exon #True
-                if position.max_base - 24 > all_exons[curr_exon][1]:
-                    in_exon = False
-                    curr_exon += 1
-        else:
-            if position.max_base - 24 < all_exons[curr_exon][1]:
-                exon_dict[name] = in_exon #True
-            else:
-                exon_dict[name] = in_exon #True
-                in_exon = False
-                curr_exon += 1
-    return exon_dict
