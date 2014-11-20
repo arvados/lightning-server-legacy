9bbcd5bc6e05b8e5cf768f6011ece5eafb0afbc9
sguthrie <sallyeguthrie@gmail.com>
2014-11-20 14:29:12 -0500

    Unsecret lightning/lightning

--- experimental/pylightweb/lightning/lightning/settings.py
+++ experimental/pylightweb/lightning/lightning/settings.py
@@ -18,20 +18,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-with open('/etc/lightning_key2.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = 'zf)ux5k0!&x!et-%qn09$td5h0phd@m@%ib8@=xc!m_g+0y)d)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
-ALLOWED_HOSTS = [
-    '.lightning-dev3.curoverse.com',
-    '.lightning-dev3.curoverse.com.',
-    ]
+ALLOWED_HOSTS = []
 
 STATICFILES_DIRS = (
     BASE_DIR + '/static/',
@@ -80,8 +76,7 @@ WSGI_APPLICATION = 'lightning.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-with open('/etc/lightning_key.txt') as f:
-    DBPW = f.read().strip()
+DBPW = "mypassword"
 
 DATABASES = {
     'default': {
