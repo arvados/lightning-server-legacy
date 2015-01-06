e7363359c8218af5a277712aeab501e0a727b614
sguthrie <sallyeguthrie@gmail.com>
2015-01-06 17:19:25 -0500

    Nice string formatting in error reporting. 0-indexed for query

--- experimental/pylightweb/BRCAlightning/tile_library/models.py
+++ experimental/pylightweb/BRCAlightning/tile_library/models.py
@@ -132,7 +132,8 @@ class TileVariant(models.Model):
             upper_position_int = int(upper_position_int)
         except ValueError:
             raise Exception('Position integer must be able to convert into an integer')
-        info_str = "Lower position int:" + str(lower_position_int) + ", upper position int (inclusive):" + str(upper_position_int) + ", length of sequence" + str(self.length)
+        info_str = "Lower position int is: " + str(lower_position_int) + ", upper position int (inclusive and 0-indexed) is: " + \
+            str(upper_position_int) + ", length of sequence is " + str(self.length)
         assert lower_position_int < self.length, "Expects the lower position integer to be 0-indexed and less than the length of the sequence. " + info_str
         assert upper_position_int < self.length, "Expects the upper position integer to be 0-indexed and less than the length of the sequence. " + info_str
         assert lower_position_int > -1, "Expects the lower position integer to be positive. " + info_str
