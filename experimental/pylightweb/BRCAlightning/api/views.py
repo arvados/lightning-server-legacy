0e2d7c71d177778cc27f3ee248a12b6151edad66
sguthrie <sallyeguthrie@gmail.com>
2015-01-08 13:17:46 -0500

    Lantern seems to be returning too high positions. Adding reporting to check

--- experimental/pylightweb/BRCAlightning/api/views.py
+++ experimental/pylightweb/BRCAlightning/api/views.py
@@ -246,6 +246,9 @@ class PopulationVariantQuery(APIView):
                     num_positions_spanned = 0
                 non_spanning_cgf_string = cgf_string.split('+')[0]
                 tile_position_int = int(string.join(non_spanning_cgf_string.split('.')[:-1], ''),16)
+                tile_position_str = basic_fns.get_position_string_from_position_int(tile_position_int)
+                assert last_tile_position_int >= tile_position_int, "cgf string went over expected max position (phase A) (%s, %s)" % (tile_position_str,
+                    basic_fns.get_position_string_from_position_int(last_tile_position_int))
                 assert len(cgf_translator) > tile_position_int-first_tile_position_int, "Translator doesn't include enough positions (phase A problem) (%i, %i)" % (len(cgf_translator), tile_position_int-first_tile_position_int)
                 if tile_position_int+num_positions_spanned >= first_tile_position_int:
                     if tile_position_int - first_tile_position_int < 0:
@@ -254,7 +257,7 @@ class PopulationVariantQuery(APIView):
                         curr_ending_tag = human_sequence_dict[human]['A'][-TAG_LENGTH:]
                         new_starting_tag = cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string][:TAG_LENGTH]
                         assert curr_ending_tag.endswith(new_starting_tag), "phase A tags do not match for human %s at position %s \n %s \n %s" % (human,
-                            basic_fns.get_position_string_from_position_int(tile_position_int), curr_ending_tag, new_starting_tag)
+                            tile_position_str, curr_ending_tag, new_starting_tag)
                         human_sequence_dict[human]['A'] += cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string][TAG_LENGTH:]
                     else:
                         human_sequence_dict[human]['A'] = cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string]
@@ -266,6 +269,9 @@ class PopulationVariantQuery(APIView):
                     num_positions_spanned = 0
                 non_spanning_cgf_string = cgf_string.split('+')[0]
                 tile_position_int = int(string.join(non_spanning_cgf_string.split('.')[:-1],''),16)
+                tile_position_str = basic_fns.get_position_string_from_position_int(tile_position_int)
+                assert last_tile_position_int >= tile_position_int, "cgf string went over expected max position (phase B) (%s, %s)" % (tile_position_str,
+                    basic_fns.get_position_string_from_position_int(last_tile_position_int))
                 assert len(cgf_translator) > tile_position_int-first_tile_position_int, "Translator doesn't include enough positions (phase B problem) (%i, %i)" % (len(cgf_translator), tile_position_int-first_tile_position_int)
                 if tile_position_int+num_positions_spanned >= first_tile_position_int:
                     if tile_position_int - first_tile_position_int < 0:
@@ -274,7 +280,7 @@ class PopulationVariantQuery(APIView):
                         curr_ending_tag = human_sequence_dict[human]['B'][-TAG_LENGTH:]
                         new_starting_tag = cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string][:TAG_LENGTH]
                         assert curr_ending_tag.endswith(new_starting_tag), "phase B tags do not match for human %s at position %s \n %s \n %s" % (human,
-                            basic_fns.get_position_string_from_position_int(tile_position_int), curr_ending_tag, new_starting_tag)
+                            tile_position_str, curr_ending_tag, new_starting_tag)
                         human_sequence_dict[human]['B'] += cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string][TAG_LENGTH:]
                     else:
                         human_sequence_dict[human]['B'] = cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string]
