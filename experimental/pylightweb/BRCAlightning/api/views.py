82daa249c888d3c8653b83b921e2d8bb8be2c052
sguthrie <sallyeguthrie@gmail.com>
2015-01-08 12:58:43 -0500

    If tag wasn't complete, threw an error

--- experimental/pylightweb/BRCAlightning/api/views.py
+++ experimental/pylightweb/BRCAlightning/api/views.py
@@ -248,10 +248,10 @@ class PopulationVariantQuery(APIView):
                 tile_position_int = int(string.join(non_spanning_cgf_string.split('.')[:-1], ''),16)
                 if tile_position_int+num_positions_spanned >= first_tile_position_int:
                     if 'A' in human_sequence_dict[human]:
-                        assert human_sequence_dict[human]['A'][-TAG_LENGTH:] == cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string][:TAG_LENGTH], \
-                            "phase A tags do not match for human %s at position %s\n%s\n%s" % (human,
-                                basic_fns.get_position_string_from_position_int(tile_position_int), human_sequence_dict[human]['A'][-TAG_LENGTH:],
-                                cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string][:TAG_LENGTH])
+                        curr_ending_tag = human_sequence_dict[human]['A'][-TAG_LENGTH:]
+                        new_starting_tag = cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string][:TAG_LENGTH]
+                        assert curr_ending_tag.endswith(new_starting_tag), "phase A tags do not match for human %s at position %s \n %s \n %s" % (human,
+                            basic_fns.get_position_string_from_position_int(tile_position_int), curr_ending_tag, new_starting_tag)
                         human_sequence_dict[human]['A'] += cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string][TAG_LENGTH:]
                     else:
                         human_sequence_dict[human]['A'] = cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string]
@@ -265,10 +265,10 @@ class PopulationVariantQuery(APIView):
                 tile_position_int = int(string.join(non_spanning_cgf_string.split('.')[:-1],''),16)
                 if tile_position_int+num_positions_spanned >= first_tile_position_int:
                     if 'B' in human_sequence_dict[human]:
-                        assert human_sequence_dict[human]['B'][-TAG_LENGTH:] == cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string][:TAG_LENGTH], \
-                            "phase B tags do not match for human %s at position %s\n%s\n%s" % (human,
-                                basic_fns.get_position_string_from_position_int(tile_position_int), human_sequence_dict[human]['B'][-TAG_LENGTH:],
-                                cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string][:TAG_LENGTH])
+                        curr_ending_tag = human_sequence_dict[human]['B'][-TAG_LENGTH:]
+                        new_starting_tag = cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string][:TAG_LENGTH]
+                        assert curr_ending_tag.ednswith(new_starting_tag), "phase B tags do not match for human %s at position %s \n %s \n %s" % (human,
+                            basic_fns.get_position_string_from_position_int(tile_position_int), curr_ending_tag, new_starting_tag)
                         human_sequence_dict[human]['B'] = cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string][TAG_LENGTH:]
                     else:
                         human_sequence_dict[human]['B'] = cgf_translator[tile_position_int - first_tile_position_int][non_spanning_cgf_string]
