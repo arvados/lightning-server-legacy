905eb21b49832d187fc72e24b58acb8f2745ea55
sguthrie <sallyeguthrie@gmail.com>
2015-01-09 11:09:53 -0500

    Need to accept hex strings, not decimal only

--- experimental/pylightweb/BRCAlightning/api/urls.py
+++ experimental/pylightweb/BRCAlightning/api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^tilevariants/$', views.TileVariantList.as_view()),
     url(r'^tilevariants/(?P<pk>[0-9]+)/$', views.TileVariantDetail.as_view()),
     url(r'^population/$', views.PopulationVariantQuery.as_view(), name="variant_query"),
-    url(r'^loci/(?P<tile_hex_string>[0-9\.]+)/$', views.TileLocusAnnotationList.as_view(), name="locus_query"),
+    url(r'^loci/(?P<tile_hex_string>[0-9a-f\.]+)/$', views.TileLocusAnnotationList.as_view(), name="locus_query"),
 ]
 
 urlpatterns=format_suffix_patterns(urlpatterns)
