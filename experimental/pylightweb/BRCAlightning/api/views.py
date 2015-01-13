e38309dba0ca067dc1686e6f6f9a3e1209f05ec9
sguthrie <sallyeguthrie@gmail.com>
2015-01-13 11:07:10 -0500

    Forgot to change variables to fit new functions

--- experimental/pylightweb/BRCAlightning/api/views.py
+++ experimental/pylightweb/BRCAlightning/api/views.py
@@ -262,7 +262,7 @@ class PopulationVariantQuery(APIView):
         #Create cgf_translator for each position
         center_cgf_translator, cgf_translator_by_position = query_fns.get_cgf_translator_and_center_cgf_translator(locuses, target_base_int, center_tile_position_int-first_tile_position_int, max_num_spanning_tiles)
 
-        return first_tile_position_int, last_tile_position_int, center_tile_position, cgf_translator_by_position, center_cgf_translator
+        return first_tile_position_int, last_tile_position_int, cgf_translator_by_position, center_cgf_translator
 
     def helper_get_bases_forward(self, curr_sequence, cgf_string, translator, num_bases_around):
         non_spanning_cgf_string = cgf_string.split('+')[0]
@@ -338,7 +338,7 @@ class PopulationVariantQuery(APIView):
                 break
         return reverse_sequence + forward_sequence[1:]
 
-    def get_population_sequences(self, first_tile_position_int, last_tile_position_int, cgf_translator, center_cgf_translator):
+    def get_population_sequences(self, first_tile_position_int, last_tile_position_int, cgf_translator, center_cgf_translator, num_bases_around):
         humans = query_fns.get_population_sequences_over_position_range(first_tile_position_int, last_tile_position_int)
         human_sequence_dict = {}
         for human in humans:
@@ -346,10 +346,10 @@ class PopulationVariantQuery(APIView):
             human_sequence_dict[human] = ['', '']
             human_sequence_dict[human][0] = self.get_bases_for_human(short_name, humans[human][0],
                                                                      first_tile_position_int, last_tile_position_int,
-                                                                     cgf_translator, center_cgf_translator)
+                                                                     cgf_translator, center_cgf_translator, num_bases_around)
             human_sequence_dict[human][1] = self.get_bases_for_human(short_name, humans[human][1],
                                                                      first_tile_position_int, last_tile_position_int,
-                                                                     cgf_translator, center_cgf_translator)
+                                                                     cgf_translator, center_cgf_translator, num_bases_around)
         humans_with_sequences = []
         for human in human_sequence_dict:
             humans_with_sequences.append(
@@ -367,16 +367,18 @@ class PopulationVariantQuery(APIView):
                 target_base = int(query_serializer.data['target_base'])
                 if query_serializer.data['indexing'] == 1:
                     target_base -= 1
-                first_tile_position_int, last_tile_position_int, max_num_spanning_tiles, cgf_translator = self.get_variants_and_bases(
+                first_tile_position_int, last_tile_position_int, cgf_translator_by_position, center_cgf_translator
+                first_tile_position_int, last_tile_position_int, cgf_translator, center_cgf_translator = self.get_variants_and_bases(
                     int(query_serializer.data['assembly']),
                     int(query_serializer.data['chromosome']),
                     target_base,
                     int(query_serializer.data['number_around']))
-                humans_and_sequences = self.get_population_sequences(first_tile_position_int, last_tile_position_int, max_num_spanning_tiles, cgf_translator)
+                humans_and_sequences = self.get_population_sequences(first_tile_position_int, last_tile_position_int,
+                    cgf_translator, center_cgf_translator, int(query_serializer.data['number_around']))
             except AssertionError as e:
                 return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            #except Exception as e:
-            #    return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            except Exception as e:
+                return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             return_serializer = PopulationVariantSerializer(data=humans_and_sequences, many=True)
             if return_serializer.is_valid():
                 return Response(return_serializer.data)
