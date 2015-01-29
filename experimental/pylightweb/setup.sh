8d58dee4cec6b08394e9c9de0a25722f98451ce9
sguthrie <sallyeguthrie@gmail.com>
2015-01-29 10:50:19 -0500

    Trying to set up linking containers support

--- experimental/pylightweb/setup.sh
+++ experimental/pylightweb/setup.sh
@@ -1,10 +1,5 @@
-apt-get install -y python-psycopg2
-
 gosu postgres postgres --single <<- EOSQL
   CREATE USER lightning WITH PASSWORD 'mypassword' CREATEDB;
   CREATE DATABASE lightning;
   GRANT ALL PRIVILEGES ON DATABASE lightning TO lightning;
 EOSQL
-
-cd /home/lightning/lightning/experimental/pylightweb/lightning
-git pull
