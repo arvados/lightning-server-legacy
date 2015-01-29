b8411512e7f46cd4e1adada451842a2f12c47dde
sguthrie <sallyeguthrie@gmail.com>
2015-01-29 12:36:34 -0500

    Still fluxing with settings

--- experimental/pylightweb/setup.sh
+++ experimental/pylightweb/setup.sh
@@ -1,5 +1,2 @@
-gosu postgres postgres --single <<- EOSQL
-  CREATE USER lightning WITH PASSWORD 'mypassword' CREATEDB;
-  CREATE DATABASE lightning;
-  GRANT ALL PRIVILEGES ON DATABASE lightning TO lightning;
-EOSQL
+echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+echo "listen_addresses='*'" >> /etc/postgresql/9.3/main/postgresql.conf
